115cfc0ad148a0205492fdb1cabc78ca44068eb3
curiousdragon <jamie4hong7@gmail.com>
2017-05-15 08:35:03 -0700

    created SpikeGenerator.java

---
+++ app/src/main/java/com/example/jamiehong/jumpgame/SpikeGenerator.java
@@ -0,0 +1,13 @@
+package com.example.jamiehong.jumpgame;
+
+import android.opengl.Matrix;
+import android.os.SystemClock;
+
+public class SpikeGenerator {
+    public SpikeGenerator(){}
+
+    public void draw(float[] mvpMatrix) {
+    }
+
+}
+
