374cd042df79a56e28a239a9bc68c51e0dfb6f34
CorAuri <jenniferlilili76458@gmail.com>
2017-05-19 08:41:39 -0700

    Update SpikeGenerator.java

--- app/src/main/java/com/example/jamiehong/jumpgame/SpikeGenerator.java
+++ app/src/main/java/com/example/jamiehong/jumpgame/SpikeGenerator.java
@@ -3,11 +3,33 @@ package com.example.jamiehong.jumpgame;
 import android.opengl.Matrix;
 import android.os.SystemClock;
 
+import java.util.ArrayList;
+
+
 public class SpikeGenerator {
-    public SpikeGenerator(){}
+    public static ArrayList<Spike> list;
+
+    public class Pair {
+        public Spike spike;
+        public float[] coord;
+
+        public Pair (Spike s, float[] f) {
+            spike = s;
+            coord = f;
+        }
+    }
+
+    public SpikeGenerator(){
+        list = new ArrayList<Spike>();
+    }
+
 
     public void draw(float[] mvpMatrix) {
+        if ((int)(Math.random() * 10) == 5) {
+            list.add(new Spike());
+        }
     }
 
 }
+
 
