b796a752e4331da911b872afc9dfe69cc6e0d1a6
curiousdragon <jamie4hong7@gmail.com>
2017-05-30 08:37:47 -0700

    added comments, deleted SpikeGenerator, changed 0.01f to Spike.velocity in GLRenderer

--- app/src/main/java/com/example/jamiehong/jumpgame/GameOverActivity.java
+++ app/src/main/java/com/example/jamiehong/jumpgame/GameOverActivity.java
@@ -7,10 +7,12 @@ import android.view.View;
 import android.widget.TextView;
 
 public class GameOverActivity extends AppCompatActivity {
+    // activity for ending screen of game
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // set the view
         setContentView(R.layout.activity_game_over);
 
         // Get the Intent that started this activity and extract the string
@@ -23,11 +25,15 @@ public class GameOverActivity extends AppCompatActivity {
     }
 
     public void restartGame(View view) {
+        // start playing the game again
+        // create an Intent that starts MainActivity
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
     public void returnToHome(View view) {
+        // return to main menu: StartGameActivity
+        // create an Intent that starts StartGameActivity
         Intent intent = new Intent(this, StartGameActivity.class);
         startActivity(intent);
     }
--- app/src/main/java/com/example/jamiehong/jumpgame/MainActivity.java
+++ app/src/main/java/com/example/jamiehong/jumpgame/MainActivity.java
@@ -10,38 +10,51 @@ import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
-    private GLSurfaceView mGLView;
-    private MediaPlayer mediaPlayer;
+    // declare variables
+    private GLSurfaceView mGLView; // used to display graphics
+    private MediaPlayer mediaPlayer; // used to play music
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // create
         super.onCreate(savedInstanceState);
+        // declare mGLView
         mGLView = new MyGLSurfaceView(this);
+        // set the content view using OpenGL ES
         setContentView(mGLView);
 
-        ///*
+        // create a media player
+        // it plays Doctor Gradus ad Parnassum, by Debussy
+        // from online free source as mp3
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.debussy_gradus);
+        // start playing the music once gameplay starts
         mediaPlayer.start();
-        //*/
     }
 
+    // resume method
     @Override
     protected void onResume() {
         super.onResume();
     }
 
+    // pause method
     @Override
     protected void onPause() {
         super.onPause();
-        ///*
+        // stop the mediaPlayer
         mediaPlayer.stop();
         mediaPlayer.release();
-        //*/
-        if(((MyGLSurfaceView) mGLView).endGameCall()) {
+
+        // call the endGame method
+        // pass in parameter how many spikes player has jumped over
+        if(((MyGLSurfaceView) mGLView).endGameCall()) { // if the user has collided
+            // then end the game
             endGame(((MyGLSurfaceView) mGLView).mRenderer.countSpikes);
         }
     }
 
+    // used in endGame()
+    // used in the intent that starts the GameOverActivity
     public static final String EXTRA_MESSAGE = "com.example.jamiehong.MESSAGE";
 
     public void endGame(int countSpikes) {
@@ -58,9 +71,9 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // encouragement
-        if(countSpikes < 10) {
+        if(countSpikes < 8) {
             message += " Better luck next time!";
-        } else if(countSpikes < 20) {
+        } else if(countSpikes < 16) {
             message += " Good job! Keep it up.";
         } else {
             message += " Wow! What a pro :)";
@@ -72,7 +85,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    // surface view class for using GLRenderer
     class MyGLSurfaceView extends GLSurfaceView {
+        // GLRenderer variable
         private final GLRenderer mRenderer;
 
         public MyGLSurfaceView(Context context) {
@@ -81,43 +96,49 @@ public class MainActivity extends AppCompatActivity {
             // Create an OpenGL ES 2.0 context
             setEGLContextClientVersion(2);
 
+            // declare mRenderer as a GLRenderer
             mRenderer = new GLRenderer(context);
 
             // Set the Renderer for drawing on the GLSurfaceView
             setRenderer(mRenderer);
-
-            // Render the view only when there is a change in the drawing data
-            //setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
-
-            // Render the view constantly (default)
-            //setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
         }
 
         @Override
         public boolean onTouchEvent(MotionEvent e) {
+            // if the user taps the screen
             if(e.getAction() == MotionEvent.ACTION_DOWN) {
+                // if the user's previous tap is no longer in effect
                 if(!mRenderer.isDuringTap()) {
+                    // set the velocity of the player green square
+                    // using the constant in GLRenderer
                     mRenderer.setVelocity(GLRenderer.PLAYER_VELOCITY);
-                    //mRenderer.setStartTime(SystemClock.elapsedRealtime());
+                    // set the start time of the tap as the current time
                     mRenderer.setStartTime(System.currentTimeMillis());
-                    //mRenderer.setStartTime(SystemClock.uptimeMillis());
+                    // makes sure that the next tap will not make
+                    // the player double jump
+                    // while this tap is still in effect
                     mRenderer.setDuringTap(true);
                 }
             }
-            //requestRender(); //can be used with RENDERMODE_WHEN_DIRTY
             return true;
         }
 
+        // used to determine when to call endGame()
         public boolean endGameCall() {
+            // if the player has collided with a spike
+            // then end the game
+            // otherwise, keep playing
             return mRenderer.hasCollided;
         }
 
+        // pause method
         @Override
         public void onPause() {
             super.onPause();
             mRenderer.onPause();
         }
 
+        // resume method
         @Override
         public void onResume() {
             super.onResume();
