cbd46f1499d49334c2e1e54c9fc54536e2bdfb6c
CorAuri <jenniferlilili76458@gmail.com>
2017-05-30 08:24:10 -0700

    Added comments to StartGame

--- app/src/main/java/com/example/jamiehong/jumpgame/StartGameActivity.java
+++ app/src/main/java/com/example/jamiehong/jumpgame/StartGameActivity.java
@@ -7,12 +7,14 @@ import android.view.View;
 
 public class StartGameActivity extends AppCompatActivity {
 
+    // Method created on creation of class
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_game);
     }
-    
+
+    // Creates an Intent
     // Start the MainActivity, which begins the game
     public void StartGame(View view) {
         Intent intent = new Intent(this, MainActivity.class);
